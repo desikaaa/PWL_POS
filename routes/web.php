6d336508cacd83976a241e9148c3a0b9f58b24e3
desikaaa <desikarmila211@gmail.com>
2025-05-16 16:54:57 +0700

    praktikum 5 done

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\PosController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 
 /*
@@ -25,6 +26,8 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+
 
 // Halaman Home projek sebelumnya
 Route::get('/', [HomeController::class, 'index'])->name('home');
