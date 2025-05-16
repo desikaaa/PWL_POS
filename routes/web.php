1837d6bcf07ff9002917e75537f9de85f97e71cd
desikaaa <desikarmila211@gmail.com>
2025-05-16 18:45:58 +0700

    praktikum 6 done

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
 
 
 // Halaman Home projek sebelumnya
